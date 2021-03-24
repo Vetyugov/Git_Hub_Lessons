7788cc15ff7603b628a88c7c899b6a4f9efdc26d
Nikita Vetyugov <nickitavet1998@gmail.com>
2021-03-24 22:27:21 +0300

    Homework. Lesson 4. Сделано 4 задание. Доработан ИИ

--- src/main/java/Lesson_4/Task_3.java
+++ src/main/java/Lesson_4/Task_3.java
@@ -40,7 +40,7 @@ public class Task_3 {
                     if (checkWin(DOT_X)) {
                         System.out.println("Вы победили!!!");
                         break;
-                    };
+                    }
                 }
                 aiTurn();
                 System.out.println("Ход ИИ");
@@ -94,23 +94,136 @@ public class Task_3 {
             mas[x-1][y-1] = DOT_X;
         }
 
-        //Ход ИИ
-        public static void aiTurn(){
+    //Ход ИИ
+    public static void aiTurn(){
+        int x = -1, y = -1;
+        int countD = 0; //счетчик подряд идущих checkDot на главных диагоналях
+        int countD1 = 0;//счетчик подряд идущих checkDot на главных диагоналях
+        int countDD = 0; //счетчик подряд идущих checkDot на побочных диагоналях
+        int countDD1 = 0;//счетчик подряд идущих checkDot на побочных диагоналях
+        //проверка диагональных элементов
+        for (int j = 0; j < 1+(SIZE-SIZEWIN); j++) {
+            for (int i = 0; i < SIZE-j; i++) {
+                //Выше главной диагонали
+                if(mas[i+j][i] == DOT_X) {
+                    countD++;
+                    if (countD == SIZEWIN-1 && isCellValid(i+j+1, i+1)) {
+                        x = i+j+1;
+                        y = i+1;
+                        break;
+                        //попробуем поставить 0 с другой стороны
+                    } else if (countD == SIZEWIN-1 && isCellValid(i+j-3, i-3)){
+                        x = i+j-3;
+                        y = i-3;
+                        break;
+                    }
+                }
+                else countD = 0;
+                //Ниже главной диагонали
+                if (mas[i][i+j] == DOT_X){
+                    countD1 ++;
+                    if (countD1 == SIZEWIN-1 && isCellValid(i+1, i+j+1)) {
+                        x = i+1;
+                        y = i+j+1;
+                        break;
+                        //попробуем поставить 0 с другой стороны
+                    } else if (countD == SIZEWIN-1 && isCellValid(i-3, i+j-3)){
+                        x = i-3;
+                        y = i+j-3;
+                        break;
+                    }
+                } else countD1 = 0;
+                //Выше побочной диагонали
+                if(mas[i+j][SIZE-i-1] == DOT_X) {
+                    countDD++;
+                    if (countDD == SIZEWIN-1 && isCellValid(i+j+1, SIZE-i-2)) {
+                        x = i+j+1;
+                        y = SIZE-i-2;
+                        break;
+                        //попробуем поставить 0 с другой стороны
+                    }else if (countD == SIZEWIN-1 && isCellValid(i+j-3, SIZE-i+2)){
+                        x = i+j-3;
+                        y = SIZE-i+2;
+                        break;
+                    }
+                }
+                else countDD = 0;
+                //Ниже побочной диагонали
+                if (mas[i][SIZE-(i+j)-1] == DOT_X){
+                    countDD1 ++;
+                    if (countDD1 == SIZEWIN-1 && isCellValid(i+1, SIZE-(i+j)-2)) {
+                        x = i+1;
+                        y = SIZE-(i+j)-2;
+                        break;
+                        //попробуем поставить 0 с другой стороны
+                    }else if (countD == SIZEWIN-1 && isCellValid(i-3, SIZE-(i+j)+2)){
+                        x = i+1;
+                        y = SIZE-(i+j)+2;
+                        break;
+                    }
+                } else countDD1 = 0;
+            }
+            countD = 0;
+            countDD = 0;
+            countDD1 = 0;
+            countD1 = 0;
+            if (x != -1 && y !=-1) break;
+        }
+        //Проверим строки и столбцы
+        if ((x == -1) && (y == -1)) {
+            //проверка по строкам и столбцам
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (mas[i][j] == DOT_X) {
+                        countD++;
+                        if(countD == SIZEWIN-1 && isCellValid(i, j+1)){
+                            x = i;
+                            y = j+1;
+                            break;
+                        } else if(countD == SIZEWIN-1 && isCellValid(i, j-3)){
+                            x = i;
+                            y = j-3;
+                            break;
+                        }
+                    }
+                    else countD = 0;
+                    if (mas[j][i] == DOT_X) {
+                        countDD++;
+                        if (countDD == SIZEWIN-1 && isCellValid(j+1, i)) {
+                            x = j+1;
+                            y = i;
+                            break;
+                        } else if (countDD == SIZEWIN-1 && isCellValid(j-3, i)) {
+                            x = j-3;
+                            y = i;
+                            break;
+                        }
+                    }
+                    else countDD = 0;
+                }
+                countD = 0;
+                countDD = 0;
+            }
+        }
+        if ((x == -1) && (y == -1)){
             Random random = new Random();
-            int x, y;
-            do {
+            do{
                 x = random.nextInt(SIZE);
                 y = random.nextInt(SIZE);
             } while (!isCellValid(x, y));
-            mas[x][y] = DOT_O;
         }
+        mas[x][y] = DOT_O;
+    }
 
-        //Проверка корректности введённых данных
+
+
+
+//Проверка корректности введённых данных
         private static boolean isCellValid(int x, int y){
             if (x>=0 && x<SIZE && y>=0 && y<SIZE){
-                if (mas[x][y]==DOT_EMPTY){
+                if (mas[x][y]==DOT_EMPTY)
                     return true;
-                } else return false;
+                 else return false;
             } else return false;
         }
 
