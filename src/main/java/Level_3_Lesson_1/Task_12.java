99f8c59069c652facbdca0a7747adaf0b5a93d75
Nikita Vetyugov <nickitavet1998@gmail.com>
2021-05-29 11:16:35 +0300

    Homework. Java part 3. Lesson 1 Исправлено, согласно замечаниям. Методы реализованы в том же классе, что и main.

--- src/main/java/Level_3_Lesson_1/Task_12.java
+++ src/main/java/Level_3_Lesson_1/Task_12.java
@@ -12,25 +12,56 @@ public class Task_12 {
 
 
         //проверка для Integer
-        GenericClass<Integer> forIntChange = new GenericClass<>();
-        forIntChange.printArray(intArray);
-        forIntChange.change(intArray, 1, 2);
-        forIntChange.printArray(intArray);
+        printArray(intArray);
+        change(intArray, 1, 2);
+        printArray(intArray);
 
         //проверка для String
-        GenericClass<String> forStrChange = new GenericClass<>();
-        forStrChange.printArray(strArray);
-        forStrChange.change(strArray, 1, 2);
-        forStrChange.printArray(strArray);
+        printArray(strArray);
+        change(strArray, 1, 2);
+        printArray(strArray);
 
         //Для второго задания
-        GenericClass<String> forArrayToArrayList = new GenericClass<>();
+
         ArrayList newArrayList = new ArrayList();
         System.out.print("Array: ");
-        forArrayToArrayList.printArray(strArray);
-        newArrayList = forArrayToArrayList.toArrayList(strArray);
+        printArray(strArray);
+        newArrayList = toArrayList(strArray);
         System.out.print("ArrayList: ");
-        forArrayToArrayList.printArrayList(newArrayList);
+        xprintArrayList(newArrayList);
 
+    }
+
+    //Для первого задания
+    //Метод, который меняет два элемента массива местами
+    public static <T> void change(T[] array, int i, int j){
+        T bubble = array[i];
+        array[i]= array[j];
+        array[j]= bubble;
+    }
+
+    //Для первого задания
+    public static <T> void printArray(T[] array){
+        for (T o:array) {
+            System.out.print(o + " ");
+        }
+        System.out.println();
+    }
+
+    //Для второго задания
+    //Метод, который преобразует массив в ArrayList
+    public static <T> ArrayList<T> toArrayList(T[] array){
+        ArrayList<T> newArrayList = new ArrayList<>();
+        for (T o:array) {
+            newArrayList.add(o);
+        }
+        return newArrayList;
+    }
+
+    public static <T>void printArrayList(ArrayList<T> arrayList){
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + " ");
+        }
+        System.out.println();
     }
 }
